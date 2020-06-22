2d2ba2ad31fc5f830efa2f5e0ee9a92ad507d28d
Jacob Goldstein <contactjakefilms@gmail.com>
2020-06-22 01:10:41 -0400

    repo init

---
+++ src/components/Art.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './art.css';
+import { FaArrowCircleRight } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+
+class Art extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      record: null
+    }
+  }
+
+  handleNext = () => {
+    this.setState({ loading: true })
+    let apiKey = "c10622c0-b3ec-11ea-8f0d-21177cb1a6f5"
+    let artworkID = Math.floor(Math.random() * 234998)
+    let url = "https://api.harvardartmuseums.org/object?apikey=" + apiKey + "&page=" + artworkID + "&size=1"
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.records[0])
+        this.setState({ record: json.records[0], loading: false })
+        if (!json.records[0].primaryimageurl) {
+          this.handleNext()
+        }
+      })
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div className="loading">Gathering A New Piece...</div>
+    }
+
+    if (!this.state.record) {
+      return (
+        <div className="welcome">
+          <div className="welcome-items">
+            <h1>Welcome to a random clicks of art!</h1>
+            <FaEye className="begin-button" size="3em" onClick={this.handleNext}/>
+          </div>
+        </div>
+      )
+    }
+
+    let nameList = null
+    if (this.state.record.people) {
+      nameList = this.state.record.people.map(name => {
+        return (
+          <h2 key={name.personid}> {name.name} </h2>
+        )
+      })
+    }
+
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="art-titles">
+            <h1> {this.state.record.title} </h1>
+            {nameList}
+            <h3> {this.state.record.culture} {this.state.record.dated} </h3>
+            <h4> {this.state.record.description} </h4>
+            <h4> {this.state.record.technique} </h4>
+
+          </div>
+
+          <div className="image-container">
+            {this.state.record.primaryimageurl ? <img className="art-img" src={this.state.record.primaryimageurl} /> : <div>No Img</div>}
+          </div>
+
+        </div>
+        <FaArrowCircleRight size="2em" className="next-button" onClick={this.handleNext} />
+
+      </div>
+    )
+  }
+}
+
+export default Art;
---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react"
+import { FaInfoCircle, FaGit } from "react-icons/fa";
+import { FaWindowClose } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import './header.css';
+import Modal from "react-modal"
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            infoModal: false
+        }
+    }
+    openModal = () => {
+        this.setState({ infoModal: true })
+    }
+
+    closeModal = () => {
+        this.setState({ infoModal: false })
+    }
+
+    render() {
+        let modalStyles = {
+            content: {
+                margin: "0 auto",
+                height: "500px",
+                width: "500px",
+                textAlign: "left"
+            }
+        }
+
+        let closeBtn = {
+            float: "right",
+            cursor: "pointer"
+        }
+
+        
+        return (
+            <div className="headerStyles">
+                {Modal.setAppElement('#root')}
+                {<FaInfoCircle size="2em" className="infoBtn" onClick={this.openModal} />}
+                <Modal style={modalStyles} isOpen={this.state.infoModal}>
+                    {<FaWindowClose size="1.5em" onClick={this.closeModal} style={closeBtn}/>}
+                    <h2>Information</h2>
+                    <p> All of the information and images are provided by the
+                    Harvard Art Museums API. This project is built using React as a form
+                    of experimentation and practice. If you enjoy the project and want to view the code 
+                    the github is below as well as links to the HAM API. 
+                    </p>
+
+                    <p>Enjoy and please remember...don't touch the art.</p>
+
+                    <ul className="creditList">
+                        <li><a href="https://www.harvardartmuseums.org/">Harvard Art Museum</a></li>
+                        <li><a href="https://github.com/harvardartmuseums/api-docs">HAM API</a></li>   
+                    </ul>
+
+                    <a href="https://github.com/jakescript">{<FaGithub className="gitIco" size="3em"/>}</a>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default Header
